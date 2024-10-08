ad6685868cf6bd7c70db7998fd192a17db7911e0
Arshan Hassanzadeh <arshan.hassanzadeh@uvic.cat>
2024-10-08 19:12:56 +0200

    13 more easy way

--- 013.2.py
+++ 013.2.py
@@ -1,23 +1,32 @@
 Qual= input("Enterar un Qualitat fuera de while: ")
 num = 0
-seguir = True
-while not(Qual  == "*"  or not seguir):
+# seguir = True
+# while not(Qual  == "*"  or not seguir):
 
-    num += 1
+#     num += 1
 
-    if Qual == "A":
+#     if Qual == "A":
 
-        Qual1 = input("Enterar otra  :")
+#         Qual1 = input("Enterar otra  :")
 
-        if Qual1 == Qual:
+#         if Qual1 == Qual:
             
-            seguir = False
+#             seguir = False
             
     
-    if seguir == True:
-        Qual = input("Enterar un Qualitat while 1 : ")
+#     if seguir == True:
+#         Qual = input("Enterar un Qualitat while 1 : ")
+
+
+
+while (Qual != "*" or not(Qual != "A" or Qual_2 != "A")):
+    Qual_2 = Qual
+    Qual= input("Enterar un Qualitat fuera de while: ")
+    num +=1 
 
 print(num-1)
+
+
 
 
 
